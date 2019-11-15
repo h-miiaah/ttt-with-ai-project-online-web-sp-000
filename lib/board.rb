718b4b2a3bec6b918296a9b59740359b1436d489
Haim D <haim11293@aol.com>
2019-11-15 09:10:09 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -35,7 +35,7 @@ class Board
         cells.count{|char| char == "X" || char == "O"}
     end
 
-    def taken?
+    def taken?(input)
         position(input) == "X" || position(input) == "O"
         # or you can use this...
         # !(position(input) == " " || position(input) == "")
