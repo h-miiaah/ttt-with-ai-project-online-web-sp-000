e2d8f0a9d327967be3121148a8123ea4a2d8a4bb
Haim D <haim11293@aol.com>
2019-11-15 09:16:51 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -46,7 +46,7 @@ class Board
     end
 
     def update(input, player)
-        self.cells[input.to_i - 1] == player.token # stands for either X or O
+        cells[input.to_i - 1] == player.token # stands for either X or O
         # or you can use this...
         # position(input) == player.token
     end
